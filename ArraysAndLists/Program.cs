b5b27902c09baecb8bd35e5d47f486cd3fac6e55
StruggsJ <strjac5@gmail.com>
2023-09-04 14:56:47 -0500

    initial commit.

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            // Create two Lists of type int.
+            
+            // Name one List "evens"
+            List<int> evens = new();
+          
+            //Name the other List "odds"
+            List<int> odds = new(); 
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +27,75 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            foreach (var i in array) 
+            {
+
+                if (i % 2 == 0)
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+
+
+            }
+
+
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+
+            Console.WriteLine("Good day, I have ten numbers on display. Which ones are you interested in seeing?");
+            Console.WriteLine("");
+
+            Console.WriteLine("1 - Odd numbers.");
+            Console.WriteLine("2 - Even numbers.");
+            Console.WriteLine("Any other key - Decline");
+            
+
+            bool validInput = int.TryParse(Console.ReadLine(), out int myChoice);
+
+            while (!validInput || myChoice > 2 || myChoice <= 0)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Have a good day!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            
+            if (myChoice == 1)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Certainly! Here are all the odd numbers:");
+                Console.WriteLine("");
+                
+                foreach (var i in odds)
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Have a good day!");
+            }
+            else 
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Certainly! Here are all the even numbers:");
+                Console.WriteLine("");
+
+                foreach (var i in evens)
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Have a good day!");
+            }
+
         }
     }
 }
